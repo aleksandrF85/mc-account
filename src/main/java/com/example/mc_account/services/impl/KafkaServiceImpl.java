53ee465e6ad09dc4e879fa865468a2fbc7013238
Aleksandr <frolov85@bk.ru>
2025-04-18 16:07:26 +0300

    create kafka service

--- src/main/java/com/example/mc_account/services/impl/KafkaServiceImpl.java
+++ src/main/java/com/example/mc_account/services/impl/KafkaServiceImpl.java
@@ -9,67 +9,84 @@ import com.example.mc_account.services.AccountService;
 import com.example.mc_account.services.KafkaService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Slf4j
-@Service
-@RequiredArgsConstructor
-public class KafkaServiceImpl implements KafkaService {
+//@Slf4j
+//@Service
+//@RequiredArgsConstructor
+//public class KafkaServiceImpl  {
 
-    private final AccountService accountServiceImpl;
+//    private final AccountService accountServiceImpl;
+//
+//    @KafkaListener(topics = "${app.kafka.userRegistration}",
+//            groupId = "${app.kafka.groupId}",
+//            containerFactory = "userRegistrationEventKafkaListenerContainerFactory")
+//    @Override
+//    public void createAccountByUserRegistrationEvent(@Payload UserRegistrationEvent event,
+//                                                     @Header(value = KafkaHeaders.RECEIVED_KEY, required = false) UUID key,
+//                                                     @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+//                                                     @Header(KafkaHeaders.RECEIVED_PARTITION) Integer partition,
+//                                                     @Header(KafkaHeaders.RECEIVED_TIMESTAMP) Long timestamp) {
+//
+//        log.info("Received event: {}", event.getUserRegistration().toString());
+//        log.info("Key: {}; Partition: {}; Topic: {}; Timestamp: {}", key, partition, topic, timestamp);
+//
+//        Account account = new Account();
+//        UserRegistration userRegistration = event.getUserRegistration();
+//
+//        account.setEmail(userRegistration.getEmail());
+//        account.setFirstName(userRegistration.getFirstName());
+//        account.setLastName(userRegistration.getLastName());
+//        account.setId(UUID.fromString(userRegistration.getUserId()));
+//        account.setRegDate(LocalDateTime.now());
+//        account.setDeleted(false);
+//        account.setBlocked(false);
+//        account.setOnline(false);
+//        account.setPassword(userRegistration.getUserId()); //TODO Уточнить откуда получать пароль
+//        log.info(account.getPassword());
+//
+//        accountServiceImpl.create(account);
+//
+//    }
+//
+//    @KafkaListener(topics = "${app.kafka.resetPassword}",
+//            groupId = "${app.kafka.groupId}",
+//            containerFactory = "resetPasswordEventKafkaListenerContainerFactory")
+//    @Override
+//    public void resetPassword(ResetPasswordEvent event) {
+//
+//        ResetPassword resetPassword = event.getResetPassword();
+//
+//        Account account = accountServiceImpl.findByEmail(resetPassword.getEmail());
+//        account.setPassword(resetPassword.getToken()); //TODO Уточнить откуда получать пароль
+//        log.info(account.getPassword());
+//
+//        accountServiceImpl.update(account, account.getId());
+//
+//    }
 
-    @KafkaListener(topics = "${app.kafka.userRegistration}",
-            groupId = "${app.kafka.groupId}",
-            containerFactory = "userRegistrationEventKafkaListenerContainerFactory")
-    @Override
-    public void createAccountByUserRegistrationEvent(@Payload UserRegistrationEvent event,
-                                                     @Header(value = KafkaHeaders.RECEIVED_KEY, required = false) UUID key,
-                                                     @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
-                                                     @Header(KafkaHeaders.RECEIVED_PARTITION) Integer partition,
-                                                     @Header(KafkaHeaders.RECEIVED_TIMESTAMP) Long timestamp) {
+    @Component
+    @Slf4j
+    @KafkaListener(id = "class-level", topics = "multi-type")
+    class KafkaServiceImpl {
 
-        log.info("Received event: {}", event.getUserRegistration().toString());
-        log.info("Key: {}; Partition: {}; Topic: {}; Timestamp: {}", key, partition, topic, timestamp);
+        @KafkaHandler
+        void listenString(String message) {
+            log.info("KafkaHandler [String] {}", message);
+        }
 
-        Account account = new Account();
-        UserRegistration userRegistration = event.getUserRegistration();
-
-        account.setEmail(userRegistration.getEmail());
-        account.setFirstName(userRegistration.getFirstName());
-        account.setLastName(userRegistration.getLastName());
-        account.setId(UUID.fromString(userRegistration.getUserId()));
-        account.setRegDate(LocalDateTime.now());
-        account.setDeleted(false);
-        account.setBlocked(false);
-        account.setOnline(false);
-        account.setPassword(userRegistration.getUserId()); //TODO Уточнить откуда получать пароль
-        log.info(account.getPassword());
-
-        accountServiceImpl.create(account);
-
+        @KafkaHandler(isDefault = true)
+        void listenDefault(Object object) {
+            log.info("KafkaHandler [Default] {}", object);
+        }
     }
 
-    @KafkaListener(topics = "${app.kafka.resetPassword}",
-            groupId = "${app.kafka.groupId}",
-            containerFactory = "resetPasswordEventKafkaListenerContainerFactory")
-    @Override
-    public void resetPassword(ResetPasswordEvent event) {
-
-        ResetPassword resetPassword = event.getResetPassword();
-
-        Account account = accountServiceImpl.findByEmail(resetPassword.getEmail());
-        account.setPassword(resetPassword.getToken()); //TODO Уточнить откуда получать пароль
-        log.info(account.getPassword());
-
-        accountServiceImpl.update(account, account.getId());
-
-    }
-
-}
