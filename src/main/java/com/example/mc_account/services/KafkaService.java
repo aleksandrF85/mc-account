1a053929ec9743f769852ffd2d6a13a84c141ef3
Aleksandr <frolov85@bk.ru>
2025-04-19 08:52:36 +0300

    add Account Changes Event

--- src/main/java/com/example/mc_account/services/KafkaService.java
+++ src/main/java/com/example/mc_account/services/KafkaService.java
@@ -1,9 +1,12 @@
 package com.example.mc_account.services;
 
+import com.skillbox.auth.dto.events.AccountChangesEvent;
 import com.skillbox.auth.dto.events.ResetPasswordEvent;
 import com.skillbox.auth.dto.events.UserRegistrationEvent;
 
 public interface KafkaService {
+
+    void sendUserRegistrationEvent(AccountChangesEvent event);
 
     void createAccountByUserRegistrationEvent(UserRegistrationEvent event);
 
